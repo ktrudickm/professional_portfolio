4e79b30692748979fcd5bc45308ce2a3d420785c
Kendall <ktrudickm@gmail.com>
2023-10-16 17:43:03 -0400

    Added Layout, Sidebar, and Home components and styling. Need to revise color scheme and minor styling

---
+++ src/components/Home/index.js
@@ -0,0 +1,22 @@
+import './index.scss'
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div className='container home-page'>
+        <div className='text-zone'>
+            <h1>Hi, <br /> I'm
+            <img src={LogoTitle} alt='developer'/>
+            lobodan
+            <br />
+            Software Engineer
+            </h1>
+            <h2>Software Engineer / Python / Javascript</h2>
+            <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Home
