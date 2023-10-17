850dd1a4a2a2f2d4a7a5e3f12a2e687a0447f687
Kendall <ktrudickm@gmail.com>
2023-10-17 11:24:31 -0400

    Added animations to letters on homepage. Added personal logo and svg files.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,41 @@
 import './index.scss'
-import LogoTitle from '../../assets/images/logo-s.png'
+// import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
+import Logo from './Logo';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = [' ','K','e','n','d','a','l','l',',']
+    const jobArray = ['s','o','f','t','w','a','r','e',' ','e','n','g','i','n','e','e','r','.']
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000)
+    })
+
   return (
     <div className='container home-page'>
         <div className='text-zone'>
-            <h1>Hi, <br /> I'm Kendall,
-            {/* <img src={LogoTitle} alt='developer'/> */}
+            <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
             <br />
-            software engineer.
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            {/* <img src={LogoTitle} alt='developer'/> */}
+            <AnimatedLetters letterClass={letterClass} 
+            strArray={nameArray}
+            idx={15} />
+            <br />
+            <AnimatedLetters letterClass={letterClass} 
+            strArray={jobArray}
+            idx={22} />
             </h1>
             <h2>Full Stack Developer / Los Angeles, CA</h2>
             <ul>
@@ -46,6 +69,7 @@ const Home = () => {
            </ul>
             <Link to='/contact' className='flat-button'>CONTACT ME</Link>
         </div>
+        <Logo />
     </div>
   )
 }
